b9eb5d7e2bfbc0fde77e1c5ff154d2bef7fec820
emil aberg <emil.aberg@elev.ga.ntig.se>
2022-04-13 13:48:10 +0200

    api

--- src/_data/github.js
+++ src/_data/github.js
@@ -1,7 +1,8 @@
 const EleventyFetch = require("@11ty/eleventy-fetch");
 
 module.exports = async function() {
-  let url = "https://api.github.com/users/aberg2";
+//  let url = "https://api.github.com/users/aberg2";
+    let url = "https://quackermeeps.herokuapp.com/meeps/api/";
 
   /* This returns a promise */
   return EleventyFetch(url, {
